20996975c3233064a5ddce83c9ed1faac93dcf8c
Tom Waller <tom.waller@versent.com.au>
2022-09-08 23:27:58 +1000

    Postgres RPC spikes

--- database/scripts/004_messages.sql
+++ database/scripts/004_messages.sql
@@ -11,7 +11,7 @@ create table public.messages (
 create trigger messages_handle_updated_at before update on public.messages
   for each row execute procedure moddatetime (updated_at);
 
-insert into public.messages (creator_id, message) values ('a66e306c-ed99-42a3-9667-9635e7294d7c', 'Test Message');
+insert into public.messages (creator_id, message) values ('f1098cb2-e607-49bb-95cb-1ac2a204af1b', 'Test Message');
 
 -- 2. Enable RLS
 alter table public.messages
@@ -22,3 +22,19 @@ create policy "Messages profiles are viewable by themselves."
   on public.messages for select using (
     auth.uid() = creator_id
 );
+
+CREATE POLICY any_user_can_create_message 
+  ON public.messages FOR INSERT
+  WITH CHECK( true );
+
+CREATE POLICY user_can_delete_own_message
+   ON public.messages FOR DELETE
+   USING(auth.uid() = creator_id);
+
+CREATE POLICY user_can_update_own_message
+   ON public.messages FOR UPDATE
+   USING(auth.uid() = creator_id);
+
+
+
+-- select * from pg_policies
